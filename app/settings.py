342d572896c0e88ec8e1863adc8a8cd0304e6ca0
iam8617477 <iam8617477@gmail.com>
2025-01-20 11:33:06 +0300

    add HOST_NAME env to configs

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG'] == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ['HOST_NAME']]
 
 
 # Application definition

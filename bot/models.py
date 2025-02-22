c6104dc12dc90d10b822e75191b82c5d5ceb7a20
iam8617477 <iam8617477@gmail.com>
2025-02-23 01:25:26 +0300

    update user model

--- bot/models.py
+++ bot/models.py
@@ -13,7 +13,10 @@ class TelegramUser(TimestampedModel):
     telegram_id = models.BigIntegerField(unique=True)
     username = models.CharField(max_length=150, blank=True, null=True)
     first_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True, blank=True, null=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
-    def __str__(self):
-        return f"{self.first_name} ({self.telegram_id})"
-
+    @property
+    def balance(self):
+        return 0
